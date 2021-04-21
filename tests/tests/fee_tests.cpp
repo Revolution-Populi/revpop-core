e91255a37c7ed9c83b26bf3494ef5d182e38a07d
tedthecreator <72611551+tedthecreator@users.noreply.github.com>
2021-04-21 17:02:37 +0300

    Remove HARDFORK_563

--- tests/tests/fee_tests.cpp
+++ tests/tests/fee_tests.cpp
@@ -598,8 +598,6 @@ BOOST_AUTO_TEST_CASE( stealth_fba_test )
       ACTORS( (alice)(bob)(chloe)(dan)(izzy)(philbin)(tom) );
       upgrade_to_lifetime_member(philbin_id);
 
-      generate_blocks( HARDFORK_563_TIME );
-
       // Philbin (registrar who registers Rex)
 
       // Izzy (initial issuer of stealth asset, will later transfer to Tom)

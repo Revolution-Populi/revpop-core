d426d8a83ab33b587b9a3fe335eb44c9919ca1a2
GoodWien <89968171+GoodWien@users.noreply.github.com>
2022-02-17 14:42:03 +0200

    Add content_cards_plugin to available plugins for witness_node

--- programs/witness_node/main.cpp
+++ programs/witness_node/main.cpp
@@ -32,6 +32,7 @@
 #include <graphene/es_objects/es_objects.hpp>
 #include <graphene/api_helper_indexes/api_helper_indexes.hpp>
 #include <graphene/custom_operations/custom_operations_plugin.hpp>
+#include <graphene/content_cards/content_cards.hpp>
 
 #include <fc/thread/thread.hpp>
 #include <fc/interprocess/signals.hpp>
@@ -94,6 +95,7 @@ int main(int argc, char** argv) {
       auto es_objects_plug = node->register_plugin<es_objects::es_objects_plugin>();
       auto api_helper_indexes_plug = node->register_plugin<api_helper_indexes::api_helper_indexes>();
       auto custom_operations_plug = node->register_plugin<custom_operations::custom_operations_plugin>();
+      auto content_cards_plug = node->register_plugin<content_cards::content_cards_plugin>();
 
       // add plugin options to config
       try
